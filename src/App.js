fa85080a2779299651d64e326ea808f52d4c50d0
Alexander Boneham <alexboneham@gmail.com>
2022-06-29 12:47:10 -0400

    set up React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import CategoriesMenu from './components/categories-menu/categories-menu.component';
-import categories from './data/categories.json';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
 
 const App = () => {
   return (
-    <CategoriesMenu categories={categories} />
-  )
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route
+        path="*"
+        element={
+          <main>
+            <h1>Something Went Wrong!</h1>
+            <p>No routes match this path...</p>
+          </main>
+        }
+      />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,10 @@
+import CategoriesMenu from '../../components/categories-menu/categories-menu.component';
+import categories from '../../data/categories.json';
+
+const Home = () => {
+  return (
+    <CategoriesMenu categories={categories} />
+  )
+};
+
+export default Home;
