684291a93e8cfce9a1b5f36ff734835f3921edbc
Alexander Boneham <alexboneham@gmail.com>
2022-11-02 15:01:02 -0400

    create memoized selectors for categories

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired');
-  return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from 'reselect';
+
+// Create variable for the desired reducer
+const selectCategoryReducer = (state) => state.categories;
+
+// Create memoized selector for getting the categories
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// Create memoized selector for the mapping of categories
+export const selectCategoriesMap = createSelector([selectCategories], (categories) =>
+  categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {});
-};
+  }, {})
+);
